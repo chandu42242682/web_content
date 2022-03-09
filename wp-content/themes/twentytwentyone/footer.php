ae29d8b626988087c94ca5a44cbd93fb8388ac3f
chandu42242682 <97518840+chandu42242682@users.noreply.github.com>
2022-03-09 22:48:16 +0530

    mycommit

--- wp-content/themes/twentytwentyone/footer.php
+++ wp-content/themes/twentytwentyone/footer.php
@@ -63,7 +63,7 @@
 			<div class="powered-by">
 				<?php
 				printf(
-					/* translators: %s: WordPress. */
+					/* translators: %s: chandu */
 					esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
 					'<a href="' . esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ) . '">WordPress</a>'
 				);
