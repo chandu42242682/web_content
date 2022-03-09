97ebd018f0ecb2147fe112c73bbe9ab12d009767
chandu42242682 <97518840+chandu42242682@users.noreply.github.com>
2022-03-09 23:45:48 +0530

    newcommitt

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -36,7 +36,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress chandra @ NCODEIT', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by WordPress Thirupathi', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
